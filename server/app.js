d58a4d2ca0b3d7c43ad9283677282acc0fc47c1e
safina zainab <safina.cumin@gmail.com>
2024-01-14 17:39:47 +0530

    Database connection

--- server/app.js
+++ server/app.js
@@ -1,14 +1,27 @@
 import dotenv from 'dotenv'
 import  express  from 'express'
 import cors from 'cors'
+import connectDB from './config/connectdb.js'
+import userRouter from './routes/userRouter.js'
 
+const app = express()
 dotenv.config()
 app.use(cors())
-const app = express()
+
 const port = process.env.port
+const DATABASE_URL = process.env.DATABASE_URL
+
+// connection
+connectDB(DATABASE_URL)
+
+// json
+app.use(express.json())
+
+app.use("/api/user", userRouter)
 
 app.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`)
 })
+
 
 
