6ac47e6871065c6ca8551c8b57095b708eaa69d9
safina zainab <safina.cumin@gmail.com>
2024-01-15 09:32:02 +0530

    Routes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
 import "./App.css";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import LoginRegister from "./pages/auth/LoginRegister";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<LoginRegister />} />
+            {/* <Route
+              path="sendpasswordresetemail"
+              element={<SendPasswordResetEmail />}
+            /> */}
+            {/* <Route path="reset" element={<ResetPassword />} /> */}
+          </Route>
+          {/* <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<h1>Error 404 Page not found !!</h1>} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
