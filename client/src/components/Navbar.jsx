a21200047080833424b13854d2f2a3a0f830d177
safina zainab <safina.cumin@gmail.com>
2024-01-16 09:11:33 +0530

    Logged in status

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,11 @@
 import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+import { getToken } from '../services/LocalStorageService';
+
 const Navbar = () => {
+
+  const token = getToken('token')
+
   return <>
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
@@ -9,7 +14,7 @@ const Navbar = () => {
 
           <Button component={NavLink} to='/' style={({ isActive }) => { return { backgroundColor: isActive ? '#6d1b7b' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Home</Button>
 
-          <Button component={NavLink} to='/login' style={({ isActive }) => { return { backgroundColor: isActive ? '#6d1b7b' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Login</Button>
+            {token ? <Button component={NavLink} to='/dashboard' style={({ isActive }) => { return { backgroundColor: isActive ? '#6d1b7b' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Dashboard</Button> : <Button component={NavLink} to='/login' style={({ isActive }) => { return { backgroundColor: isActive ? '#6d1b7b' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Login</Button>}
 
         </Toolbar>
       </AppBar>
